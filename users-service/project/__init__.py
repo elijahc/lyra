ecf49fa8dfd65b1da361c73670b9782d01460fd4
Elijah Christensen <ejd.christensen@gmail.com>
2018-02-08 16:15:57 -0700

    dockerized

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,12 +1,16 @@
+import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
-@app.route('/users/ping', methods=['GET'])
+@app.route('/users/ping',methods=['GET'])
 def ping_pong():
     return jsonify({
         'status': 'success',
