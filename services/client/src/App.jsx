5c373fe87bdccd9ab085ebcb8355f11198971bf8
Elijah Christensen <ejd.christensen@gmail.com>
2018-02-09 06:21:14 -0700

    db wipe

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
 import axios from 'axios';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
 
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import NavBar from './components/NavBar'
+import Form from './components/Form';
 import About from './components/About';
 
 class App extends Component {
@@ -14,7 +15,12 @@ class App extends Component {
       users: [],
       username: '',
       email: '',
-      title: 'Lyra'
+      title: 'Lyra',
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      }
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -54,24 +60,36 @@ class App extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-6">
-              <br/>
+              <br />
               <Switch>
                 <Route exact path='/' render={() => (
                   <div>
                     <h1>All Users</h1>
-                    <hr/><br/>
+                    <hr /><br />
                     <AddUser
                       username={this.state.username}
                       email={this.state.email}
                       handleChange={this.handleChange}
                       addUser={this.addUser}
                     />
-                    <br/>
-                    <UsersList users={this.state.users}/>
-                    <About/>
+                    <br />
+                    <UsersList users={this.state.users} />
+                    <About />
                   </div>
                 )} />
-                <Route exact path='/about' component={About}/>
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' render={() => (
+                  <Form
+                    formType={'Register'}
+                    formData={this.state.formData}
+                  />
+                )} />
+                <Route exact path='/login' render={() => (
+                  <Form
+                    formType={'Login'}
+                    formData={this.state.formData}
+                  />
+                )} />
               </Switch>
             </div>
           </div>
--- services/users/project/tests/test_user_model.py
+++ services/users/project/tests/test_user_model.py
@@ -12,11 +12,13 @@ from project.tests.utils import add_user
 class TestUserModel(BaseTestCase):
 
     def test_add_user(self):
-        user = add_user('justatest', 'test@test.com')
+        user = add_user('justatest', 'test@test.com', 'test')
         self.assertTrue(user.id)
         self.assertEqual(user.username, 'justatest')
         self.assertEqual(user.email, 'test@test.com')
+        self.assertTrue(user.password)
         self.assertTrue(user.active)
+        self.assertFalse(user.admin)
 
     def test_add_user_duplicate_username(self):
         add_user('justatest', 'test@test.com')
@@ -39,3 +41,8 @@ class TestUserModel(BaseTestCase):
     def test_to_json(self):
         user = add_user('justatest', 'test@test.com')
         self.assertTrue(isinstance(user.to_json(), dict))
+
+    def test_passwords_are_random(self):
+            user_one = add_user('justatest', 'test@test.com', 'test')
+            user_two = add_user('justatest2', 'test@test2.com', 'test')
+            self.assertNotEqual(user_one.password, user_two.password) 
