71c4d1d3dd43d1be97b4f9babdc52dab49c2dd16
Elijah Christensen <ejd.christensen@gmail.com>
2018-02-09 21:27:59 -0700

    trimmed a lot of fat

--- services/app/project/api/models.py
+++ services/app/project/api/models.py
@@ -1,22 +1,4 @@
-from project import db, bcrypt
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), unique=True, nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+from flask import Blueprint, jsonify, request, render_template
 
-    def to_json(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-            'active': self.active
-        }
 
-    def __init__(self, username, email,password):
-        self.username = username
-        self.email = email
-        self.password = bcrypt.generate_password_hash(password).decode()
